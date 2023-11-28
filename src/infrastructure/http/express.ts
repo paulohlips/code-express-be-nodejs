7c5b6b9a822dca194af7999cecb6b5328a87a37a
paulohlips <paulohfcmendes@gmail.com>
2023-11-28 13:54:56 -0300

    feat: apply auth middleware

--- src/infrastructure/http/express.ts
+++ src/infrastructure/http/express.ts
@@ -4,16 +4,20 @@ import FindUserController from "../../controller/UserController/FindUserControll
 import CreateUserController from "../../controller/UserController/CreateUserController"
 import CreateBlogPostController from "../../controller/BlogPostController/CreateBlogPostController"
 import FindBlogPostController from "../../controller/BlogPostController/FindBlogPostController"
+import AuthMiddleware from "../../middleware/AuthMiddleware"
+import LoginController from "../../controller/LoginController"
 
 const app = express()
 app.use(express.json())
 const port = 3000
 
+// login route
+app.post('/login', ExpressAdapter.create(LoginController.login))
+app.use(AuthMiddleware.check)
 app.post('/user', ExpressAdapter.create(CreateUserController.createUser))
 app.get('/user/:userId', ExpressAdapter.create(FindUserController.findUser))
 app.post('/post', ExpressAdapter.create(CreateBlogPostController.createBlogPost))
 app.get('/post/:postId', ExpressAdapter.create(FindBlogPostController.findBlogPost))
-
 
 app.listen(port, () => {
   `Express server running on port ${3000}`
