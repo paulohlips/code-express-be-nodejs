c7c6623f47de4878fbae622ab40b7fbb8d4317a4
paulohlips <paulohfcmendes@gmail.com>
2023-12-12 08:13:07 -0300

    feat: changes adapter do receive http status

--- src/adapter/ExpressAdapter.ts
+++ src/adapter/ExpressAdapter.ts
@@ -4,6 +4,7 @@ export default class ExpressAdapter {
   static create (fn: any) {
     return async (request: Request, response: Response) => {
       const obj = await fn(request.params, request.body)
+      response.status(obj?.httpCode || 200)
       response.json(obj)
     }
   }
