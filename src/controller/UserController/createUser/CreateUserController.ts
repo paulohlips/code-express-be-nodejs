b12752dc02f43d8af469b872aa93b949c3d9f996
paulohlips <paulohfcmendes@gmail.com>
2023-12-12 08:13:58 -0300

    feat: update user controller to apply validation

---
+++ src/controller/UserController/createUser/CreateUserController.ts
@@ -0,0 +1,38 @@
+import CreateUserUseCase from "../../../core/useCase/User/CreateUserUseCase"
+import UserRepositoryInMemory from "../../../infrastructure/repository/UserRepositoryInMemory"
+import PasswordHashService from "../../../infrastructure/service/PasswordHashService"
+import CreateUserValidationService from "./CreateUserValidation"
+import ValidationError from "../../../errors/validationError"
+
+interface InputBody {
+  username: string
+  email: string
+  password: string
+  bio?: string
+}
+
+export default class CreateUserController {
+  static async createUser (params: any, { username, email, password, bio }: InputBody ) {
+
+    try {
+      await CreateUserValidationService.validateInput({ username, email, password, bio })
+      const userRepository = new UserRepositoryInMemory()
+      const passwordHashService = new PasswordHashService()
+      const createUserUseCase = new CreateUserUseCase(userRepository, passwordHashService)
+      const result = await createUserUseCase.execute(username, email, password, bio)
+
+      return {
+        data: result
+      }
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        return {
+          statusMessage: 'error',
+          message: 'Validation failed.',
+          errors: error.errors,
+          httpCode: 400
+        }
+      }
+    }
+  }
+}
