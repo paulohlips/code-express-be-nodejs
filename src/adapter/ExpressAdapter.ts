2f0378bcaf0e55145f5778c8d960cfd68fbe8d2d
paulohlips <paulohfcmendes@gmail.com>
2023-12-13 08:24:08 -0300

    feat: add query params to express adapter

--- src/adapter/ExpressAdapter.ts
+++ src/adapter/ExpressAdapter.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express"
 export default class ExpressAdapter {
   static create (fn: any) {
     return async (request: Request, response: Response) => {
-      const obj = await fn(request.params, request.body)
+      const obj = await fn(request.params, request.body, request.query)
       response.status(obj?.httpCode || 200)
       response.json(obj)
     }
