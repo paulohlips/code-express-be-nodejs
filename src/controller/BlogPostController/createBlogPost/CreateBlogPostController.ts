03487a8305d553f81a76ec10153ca6f4cdf2bc4b
paulohlips <paulohfcmendes@gmail.com>
2023-12-13 06:58:04 -0300

    feat: apply create blog post validation

--- src/controller/BlogPostController/createBlogPost/CreateBlogPostController.ts
+++ src/controller/BlogPostController/createBlogPost/CreateBlogPostController.ts
@@ -1,6 +1,7 @@
 import CreateBlogPostUseCase from "../../../core/useCase/BlogPost/CreateBlogPostUseCase"
 import ValidationError from "../../../errors/validationError"
 import BlogPostRepositoryInMemory from "../../../infrastructure/repository/BlogPostRepositoryInMemory"
+import CreateBlogPostValidationService from "./CreateBlogPostValidation"
 
 interface Input {
   title: string
@@ -13,6 +14,7 @@ interface Input {
 export default class CreateBlogPostController {
   static async createBlogPost (params: any, { title, content, authorId, status, tags }: Input ) {
     try {
+      await CreateBlogPostValidationService.validateInput({ title, content, authorId, status, tags })
       const userRepository = new BlogPostRepositoryInMemory()
       const createBlogPostUseCase = new CreateBlogPostUseCase(userRepository)
       return await createBlogPostUseCase.execute({ title, content, authorId, status, tags })
